2d73a7337dc46927462d30d85cdb413139562b0b
Waylon Peele <waylon@cloudcliques.com>
2014-12-10 13:39:53 -0800

    put url after path

--- app/models/spree/app_configuration/variant_configuration.rb
+++ app/models/spree/app_configuration/variant_configuration.rb
@@ -2,8 +2,8 @@ module SpreeVariantOptions
   class VariantConfiguration < Spree::Preferences::Configuration
     preference :allow_select_outofstock, :boolean, :default => false
     preference :default_instock, :boolean, :default => false
-    preference :option_value_url, :string, :default => ':path'
     preference :option_value_path, :string, :default => '/spree/option_values/:id/:style/:basename.:extension'
+    preference :option_value_url, :string, :default => ':path'
     preference :option_value_styles, :string, default: "{\"small\":\"30x30#\",\"large\":\"140x110#\"}"
     preference :option_value_default_style, :string, default: 'small'
   end
