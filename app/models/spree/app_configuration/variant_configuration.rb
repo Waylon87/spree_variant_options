9107c180b085d416403ff32ea3903437a44c946f
Waylon Peele <waylon@cloudcliques.com>
2015-01-13 12:38:17 -0800

    changed prefernces path name

--- app/models/spree/app_configuration/variant_configuration.rb
+++ app/models/spree/app_configuration/variant_configuration.rb
@@ -3,7 +3,7 @@ module SpreeVariantOptions
     preference :allow_select_outofstock, :boolean, :default => false
     preference :default_instock, :boolean, :default => false
     preference :option_value_path, :string, :default => '/spree/option_values/:id/:style/:basename.:extension'
-    preference :option_value_url, :string, :default => ':path'
+    preference :option_value_url, :string, :default => ':option_value_path'
     preference :option_value_styles, :string, default: "{\"small\":\"30x30#\",\"large\":\"140x110#\"}"
     preference :option_value_default_style, :string, default: 'small'
   end
